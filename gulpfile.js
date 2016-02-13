2df136308f68e396841d64749fd5c62112bbdd13
Jouni Kantola <jouni@kantola.se>
2016-02-13 11:27:58 +0100

    File watch task

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const watch = require('gulp-watch');
+
+const config = {
+    paths: {
+        tests: ['test/**/*.js'],
+        scripts: ['src/**/*.js']   
+    }
+}
+
+gulp.task('test', function() {
+  return gulp.src(config.paths.tests, {read: false})
+    .pipe(mocha({ reporter: 'dot' }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.paths.scripts, ['test']);
+    gulp.watch(config.paths.tests, ['test']);
+});
+
+gulp.task('default', ['test', 'watch']);
