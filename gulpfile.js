3b85db90fa8ebca2a49786dc15c3e18a200ec36e
Jouni Kantola <jouni.kantola@dn.se>
2016-02-20 00:19:33 +0100

    JSHint -> ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,50 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
+const gulp = require("gulp");
 const ava = require("gulp-ava");
-const watch = require('gulp-watch');
-const jshint = require('gulp-jshint');
+const eslint = require("gulp-eslint");
 
 const config = {
     paths: {
-        tests: ["lib/**/test/*.js"],
-        scripts: ["lib/**/*.js"]
+        tests: ["lib/**/test/**/*.js", "!node_modules/**"],
+        scripts: ["lib/**/*.js", "!node_modules/**"]
     }
 };
 
-gulp.task('test', function() {
-  const files = gulp.src(config.paths.tests);
-    return files.pipe(ava());
+gulp.task("test", () => {
+    return gulp.src(config.paths.tests[0])
+        .pipe(ava());
 });
 
-gulp.task('lint', function() {
-  return gulp.src(config.paths.scripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+gulp.task("lint", () => {
+    return gulp.src(config.paths.scripts)
+        .pipe(eslint({
+            extends: "eslint:recommended",
+            parser: "babel-eslint",
+            parserOptions: {
+                ecmaVersion: 6,
+                sourceType: "module",
+                ecmaFeatures: {
+                    modules: true
+                }
+            },
+            rules: {
+                "no-undef": 2,
+                "no-console": 0,
+                "eqeqeq": 1,
+                "camelcase": 1,
+                "comma-dangle": 2,
+                "quotes": [1, "double"]
+            },
+            envs: [
+                "node",
+                "es6"
+            ]
+        }))
+        .pipe(eslint.formatEach("stylish", process.stderr));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(config.paths.scripts, ['lint', 'test']);
-    gulp.watch(config.paths.tests, ['test']);
+gulp.task("watch", () => {
+    gulp.watch(config.paths.tests, ["test"]);
+    gulp.watch(config.paths.scripts, ["lint", "test"]);
 });
 
-gulp.task('default', ['lint', 'watch']);
+gulp.task("default", ["lint", "watch"]);
